07ec03e4bd5ca038cf3aebf256d994d8198e9263
Sai Nishit Ragupathy <sr6931@nyu.edu>
2024-10-27 21:04:40 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,35 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-}
+    createCanvas(500, 500);
+    background(255); // Set background to white
+    noLoop(); // Prevent continuous drawing
+  
+    const swappedRhombuses = [
+      [{x: 100, y: 150}, {x: 250, y: 250}, {x: 100, y: 350}, {x: 50, y: 300}],
+      [{x: 400, y: 150}, {x: 250, y: 250}, {x: 400, y: 350}, {x: 450, y: 300}],
+      [{x: 250, y: 250}, {x: 100, y: 350}, {x: 250, y: 450}, {x: 200, y: 400}],
+      [{x: 250, y: 250}, {x: 400, y: 350}, {x: 250, y: 450}, {x: 300, y: 400}],
+      [{x: 175, y: 200}, {x: 250, y: 250}, {x: 325, y: 200}, {x: 250, y: 150}],
+      [{x: 175, y: 200}, {x: 250, y: 250}, {x: 150, y: 300}, {x: 125, y: 250}],
+      [{x: 325, y: 200}, {x: 250, y: 250}, {x: 350, y: 300}, {x: 375, y: 250}],
+      [{x: 175, y: 300}, {x: 250, y: 250}, {x: 325, y: 300}, {x: 250, y: 350}],
+    ];
+  
+    const swappedTriangles = [
+      [{x: 200, y: 100}, {x: 250, y: 150}, {x: 300, y: 100}],
+      [{x: 200, y: 400}, {x: 250, y: 450}, {x: 300, y: 400}],
+      [{x: 100, y: 250}, {x: 150, y: 300}, {x: 100, y: 350}],
+      [{x: 400, y: 250}, {x: 450, y: 300}, {x: 400, y: 350}],
+    ];
+  
+    swappedRhombuses.forEach(rhombus => drawPolygon(rhombus, 'black'));
+    swappedTriangles.forEach(triangle => drawPolygon(triangle, 'black'));
+  }
+  
+  function drawPolygon(points, fillColor) {
+    fill(fillColor);
+    stroke(0);
+    beginShape();
+    points.forEach(point => vertex(point.x, point.y));
+    endShape(CLOSE);
+  }
+  
