59704a12e39183faaed71d9ac89bcc13b4e8e2ca
Sai Nishit Ragupathy <sr6931@nyu.edu>
2024-11-25 15:08:37 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,35 +1,34 @@
 function setup() {
-    createCanvas(500, 500);
-    background(255); 
-    noLoop(); 
-  
-    const swappedRhombuses = [
-      [{x: 100, y: 150}, {x: 250, y: 250}, {x: 100, y: 350}, {x: 50, y: 300}],
-      [{x: 400, y: 150}, {x: 250, y: 250}, {x: 400, y: 350}, {x: 450, y: 300}],
-      [{x: 250, y: 250}, {x: 100, y: 350}, {x: 250, y: 450}, {x: 200, y: 400}],
-      [{x: 250, y: 250}, {x: 400, y: 350}, {x: 250, y: 450}, {x: 300, y: 400}],
-      [{x: 175, y: 200}, {x: 250, y: 250}, {x: 325, y: 200}, {x: 250, y: 150}],
-      [{x: 175, y: 200}, {x: 250, y: 250}, {x: 150, y: 300}, {x: 125, y: 250}],
-      [{x: 325, y: 200}, {x: 250, y: 250}, {x: 350, y: 300}, {x: 375, y: 250}],
-      [{x: 175, y: 300}, {x: 250, y: 250}, {x: 325, y: 300}, {x: 250, y: 350}],
-    ];
-  
-    const swappedTriangles = [
-      [{x: 200, y: 100}, {x: 250, y: 150}, {x: 300, y: 100}],
-      [{x: 200, y: 400}, {x: 250, y: 450}, {x: 300, y: 400}],
-      [{x: 100, y: 250}, {x: 150, y: 300}, {x: 100, y: 350}],
-      [{x: 400, y: 250}, {x: 450, y: 300}, {x: 400, y: 350}],
-    ];
-  
-    swappedRhombuses.forEach(rhombus => drawPolygon(rhombus, 'black'));
-    swappedTriangles.forEach(triangle => drawPolygon(triangle, 'black'));
-  }
-  
-  function drawPolygon(points, fillColor) {
-    fill(fillColor);
-    stroke(0);
-    beginShape();
-    points.forEach(point => vertex(point.x, point.y));
-    endShape(CLOSE);
-  }
-  
+  createCanvas(500, 500);
+  background(0); // Set background to black
+  noLoop(); // Prevent continuous drawing
+
+  const triangles = [
+    [{x: 100, y: 150}, {x: 250, y: 250}, {x: 100, y: 350}],
+    [{x: 400, y: 150}, {x: 250, y: 250}, {x: 400, y: 350}],
+    [{x: 250, y: 250}, {x: 100, y: 350}, {x: 250, y: 450}],
+    [{x: 250, y: 250}, {x: 400, y: 350}, {x: 250, y: 450}],
+    [{x: 175, y: 200}, {x: 250, y: 250}, {x: 325, y: 200}],
+    [{x: 175, y: 200}, {x: 250, y: 250}, {x: 150, y: 300}],
+    [{x: 325, y: 200}, {x: 250, y: 250}, {x: 350, y: 300}],
+    [{x: 175, y: 300}, {x: 250, y: 250}, {x: 325, y: 300}],
+  ];
+
+  const rhombuses = [
+    [{x: 200, y: 100}, {x: 250, y: 150}, {x: 300, y: 100}, {x: 250, y: 50}],
+    [{x: 200, y: 400}, {x: 250, y: 450}, {x: 300, y: 400}, {x: 250, y: 350}],
+    [{x: 100, y: 250}, {x: 150, y: 300}, {x: 100, y: 350}, {x: 50, y: 300}],
+    [{x: 400, y: 250}, {x: 450, y: 300}, {x: 400, y: 350}, {x: 350, y: 300}],
+  ];
+
+  triangles.forEach(triangle => drawPolygon(triangle, 'white'));
+  rhombuses.forEach(rhombus => drawPolygon(rhombus, 'white'));
+}
+
+function drawPolygon(points, fillColor) {
+  fill(fillColor);
+  stroke(255); // Set stroke color to white
+  beginShape();
+  points.forEach(point => vertex(point.x, point.y));
+  endShape(CLOSE);
+}
